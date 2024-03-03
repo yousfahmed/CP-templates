777a50a15939bbaac26c19741416b574bbab6f6c
abdelaleem-abdelaleem <abdelaleem.abdelaleem@bld.ai>
2024-03-03 03:26:13 +0200

    update: z function

--- Strings/Z.cpp
+++ Strings/Z.cpp
@@ -1,12 +1,20 @@
 /* the max number of characters starting from the position  
 i that coincide with the first characters of s  */
-vector<int> Z(const string &s) {
-  const int n = s.size();
-  vector<int> z(n);
-  for (int i = 1; i < n; i++) {
-    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
-      z[i]++;
+vector<int> z_function(string s) {
+    int n = s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for(int i = 1; i < n; i++) {
+        if(i < r) {
+            z[i] = min(r - i, z[i - l]);
+        }
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+        if(i + z[i] > r) {
+            l = i;
+            r = i + z[i];
+        }
     }
-  }
-  return z;
+    return z;
 }
